4dfe16bc0b9d1412192f39c1bab5ff94bea48ed3
T20 Miguel Miguel <95326609+MiguelMVil@users.noreply.github.com>
2021-12-01 11:45:47 +0000

    Smooth nav transitions already work. Need to fix the layout of nav bar

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,15 @@ makes it stick to the top of the screen once it
 intersects with the top
 */
 
-
-
-
-
 var nav = document.querySelector("#nav")
+const small_header = document.querySelector("#small-header")
 
 const observer = new IntersectionObserver(function(entries) {
 	if(entries[0].intersectionRatio < 1){
-		document.querySelector("#small-header").classList.remove("header-unstuck");
-		document.querySelector("#small-header").style.display = "flex";
+    small_header.classList.add("active")
 	}
 	else{
-		document.querySelector("#small-header").classList.add("header-unstuck");
-		document.querySelector("#small-header").style.display = "none";
+    small_header.classList.remove("active")
 	}
 }, { threshold: [1] });
 
